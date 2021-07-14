f6addbb2d321cd24cc8e1174d4dda8f6b4439311
Felipe Siqueira Pinheiro <fesiqueirapinheiro@gmail.com>
2021-07-14 18:20:30 -0300

    emacs: magit "fullscreen"

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -120,7 +120,9 @@
   :init (evil-collection-init))
 
 ;; magit
-(use-package magit)
+(use-package magit
+  :init
+  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
 
 ;; sync env variables
 (use-package exec-path-from-shell
